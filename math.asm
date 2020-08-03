92277c0cbfebc70a754e13c9703cc5ea268ae230
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-03 15:46:10 +0100

    math samply

---
+++ math.asm
@@ -0,0 +1,21 @@
+org 0x100
+
+main:
+	mov bx,integer1
+	mov cx,integer2
+	mov dx,integer3
+	mov si,4
+	mov ax,0x9
+	int 0xf0
+	mov bx,integer1
+	mov ax,0x8
+	int 0xf0
+
+
+end:
+	mov ax,0
+	int 0xF0
+msg	db 12,'hello world',13,10,0
+integer1 dd 0
+integer2 dd 1234567
+integer3 dd 100
